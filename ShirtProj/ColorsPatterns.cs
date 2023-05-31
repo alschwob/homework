c4133d97583e30c0cc0f641d2eb3380b6d33fd83
Aryeh Schwob <aryeh.schwob@gmail.com>
2023-05-31 05:49:18 +0300

    shirt project homework

---
+++ ShirtProj/ColorsPatterns.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirtProj
+{
+    public class ColorsPatterns
+    {
+        string [] colors = new string[]
+            {
+                "Red",
+                 "Green",
+                "Blue",
+                "Yellow"
+            };
+        string[] patterns = new string[]
+                    {
+                       "striped",
+                       "solid",
+                       "checkered",
+                       "tie die"
+                    };
+        public string[] GetColors()
+        {
+            return colors;
+        }
+        public string[] GetPatterns()
+        {
+            return patterns;
+        }
+    }
+}
---
+++ ShirtProj/shirt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ShirtProj
+{
+    internal class shirt
+    {
+
+        static void Main(string[] args)
+        {
+            ColorsPatterns cp = new ColorsPatterns();
+            string[] c = cp.GetColors();
+            string[] p = cp.GetPatterns();
+            createShirt(c,p);
+        }
+        
+       public static void createShirt(string[] colors, string[]patterns)
+        { 
+            Random r = new Random();
+            for (int i = 0; i < colors.Length; i++)
+            {
+                for (int j = 0; j < patterns.Length; j++)
+                {
+                   
+                    {
+                        if (patterns[j] == "solid")
+                        {
+                            string color = colors[i];
+                            string pattern = patterns[j];
+                            Console.WriteLine($"Color: {color}, Pattern: {pattern}");
+                        }
+                        else
+                        {
+                            string pattern = patterns[j];
+                            string color = colors[i];
+                            string color2 = colors[r.Next(colors.Length)];
+                                do
+                                {
+                                    color2 = colors[r.Next(colors.Length)];
+                                } while (color2 == color);
+                            Console.WriteLine($"Colors: {color} and {color2}, Pattern: {pattern}");
+                        }
+                       
+                    }
+                }
+            }
+        }
+    }
+}
